fd9b37fc8829351aad60806cdb578e3a92b52634
Roland Jablonski <rolandjablonski@Rolands-MacBook-Air.local>
2017-04-13 19:07:49 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var a = 4;
+var h = 6;
+var triangleArea = a*h/2;
+console.log(triangleArea);
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+
