ec0d5c9767443c8abbc50abbe09a36a591d727a5
friedpinch <friedpinch@gmail.com>
2017-04-18 20:19:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-var a = 4;
-var h = 6;
+var a = prompt('Podaj podstawę trójkąta');
+var h = prompt('Podaj wysokość trójkąta');
 var triangleArea = a*h/2;
-console.log(triangleArea);
 console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
-var a = prompt('4'), b = prompt('6'), value = a*h/2;
 
